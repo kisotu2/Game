f7853201f622301d7daa66858ace2154a93cec08
Samwel Kisotu <kisotusamuel2@gmail.com>
2024-11-12 18:36:38 +0300

    flipping of buttons when clicked

--- GameControler.cs
+++ GameControler.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 public class GameController : MonoBehaviour
 {
     [SerializeField]
-    private Sprite bgImage;
+    private Sprite bgImage; // Original background sprite
+    [SerializeField]
+    private Sprite flippedImage; // Sprite for the flipped state
 
     public List<Button> btns = new List<Button>();
 
@@ -22,7 +24,7 @@ public class GameController : MonoBehaviour
         for (int i = 0; i < objects.Length; i++)
         {
             btns.Add(objects[i].GetComponent<Button>());
-            btns[i].image.sprite = bgImage;
+            btns[i].image.sprite = bgImage; // Set to default background
         }
     }
 
@@ -36,6 +38,14 @@ public class GameController : MonoBehaviour
 
     public void PickAPuzzle()
     {
-        Debug.Log("You are clicking a button");
+        // Get the button that was clicked
+        GameObject clickedButton = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+        string buttonName = clickedButton.name;
+
+        // Change the sprite to show the button as "flipped"
+        clickedButton.GetComponent<Image>().sprite = flippedImage;
+
+        // Log the name of the clicked button
+        Debug.Log("You are clicking a button named " + buttonName);
     }
 }
